8d807a257dfe9b403c654f477448b6a427127921
zh92 <zhehao92@gmail.com>
2023-01-27 12:26:02 +0800

    Updated Calculator program

--- task02/src/sdf/Calculator.java
+++ task02/src/sdf/Calculator.java
@@ -8,22 +8,45 @@ public class Calculator {
         
         System.out.println("Welcome.");
         Console cons = System.console();
-        String input = cons.readLine(">");
+        String input = "";
+        Double $last = 0.00;
 
-        if (input.equalsIgnoreCase("exit")) {
-            System.out.println("Bye bye");
-        } else {
-            input.trim();
-            String[] columns = input.split(" ");
+        while (!input.equalsIgnoreCase("exit")) {
+            input = cons.readLine(">");
 
-            Double number1 = Double.parseDouble(columns[0]);
-            Double number2 = Double.parseDouble(columns[2]);
-            String operator = columns[1];
-            Double answer = 0.00;
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Bye bye");
+            } else {
+                input.trim();
+                String[] columns = input.split(" ");
+                Operator op = new Operator();
+                String operator = columns[1];
+                Double answer = 0.00;
 
-            Operator op = new Operator();
-            answer = op.evaluate(number1, operator, number2);
-            System.out.println(answer);
+                if (columns[0].equalsIgnoreCase("$last")) {
+                    Double number1 = $last;
+                    Double number2 = Double.parseDouble(columns[2]);
+                    answer = op.evaluate(number1, operator, number2);
+
+                } else if (columns[2].equalsIgnoreCase("$last")) {
+                    Double number2 = $last;
+                    Double number1 = Double.parseDouble(columns[0]);
+                    answer = op.evaluate(number1, operator, number2);
+    
+                } else if (columns[0].equalsIgnoreCase("$last") && columns[2].equalsIgnoreCase("$last")) {
+                    Double number2 = $last;
+                    Double number1 = $last;
+                    answer = op.evaluate(number1, operator, number2);
+    
+                }else {
+                    Double number1 = Double.parseDouble(columns[0]);
+                    Double number2 = Double.parseDouble(columns[2]);
+                    answer = op.evaluate(number1, operator, number2);
+                }
+                System.out.println(answer);
+                $last = answer;
+            }
+        
         }
     }
 }
